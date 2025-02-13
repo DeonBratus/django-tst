57545aa0107c51b634ccdb017adb1c3bb5b1a1c4
DaBratusPC <bratus26d@gmail.com>
2025-02-13 21:57:37 +0300

    simple_api

--- foodcode/foodapp/models.py
+++ foodcode/foodapp/models.py
@@ -1,7 +1,9 @@
 from django.db import models
-from django.db.models import *
+from model_utils.models import TimeStampedModel
+from django.db.models import CharField, SmallIntegerField,\
+    ForeignKey, BooleanField, IntegerField, DecimalField, ManyToManyField
 
-class FoodCategory():
+class FoodCategory(TimeStampedModel):
 
     name_ru = CharField(verbose_name="Название на русском", max_length=255)
     name_en = CharField(verbose_name="Название на английском", max_length=255)
@@ -18,3 +20,31 @@ class FoodCategory():
         verbose_name = "Раздел меню"
         verbose_name_plural = "Разделы меню"
         ordering = ('name_ru', 'order_id')
+
+
+class Food(TimeStampedModel):
+
+    category = ForeignKey(FoodCategory, verbose_name='Раздел меню', related_name='food', on_delete=models.CASCADE)
+    
+    is_vegan = BooleanField(verbose_name='Вегетарианское меню', default=False)
+    is_special = BooleanField(verbose_name='Специальное предложение', default=False)
+
+    code = IntegerField(verbose_name='Код поставщика')
+    internal_code = IntegerField(verbose_name='Код в приложении', unique=True, null=True, blank=True)
+
+    name_ru = CharField(verbose_name='Название на русском', max_length=255)
+    description_ru = CharField(verbose_name='Описание на русском', max_length=255, blank=True, null=True)
+    description_en = CharField(verbose_name='Описание на английском', max_length=255, blank=True, null=True)
+    description_ch = CharField(verbose_name='Описание на китайском', max_length=255, blank=True, null=True)
+
+    cost = DecimalField(verbose_name='Цена', max_digits=10, decimal_places=2)
+    
+    is_publish = BooleanField(verbose_name='Опубликовано', default=True)
+    
+    additional = ManyToManyField('self', verbose_name='Дополнительные товары', symmetrical=False,
+                                        related_name='additional_from', blank=True)
+
+
+    def __str__(self):
+        return self.name_ru
+    
--- foodcode/foodapp/views.py
+++ foodcode/foodapp/views.py
@@ -1,12 +1,23 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import serializers
-import json
+from foodapp import models
+from foodapp import serializers
 
 class FoodViewer(APIView):
     
+    def post(self, request):
+        serializer = serializers.FoodListSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
     def get(self, request):
-        data = {
-            "msg": 'message'
-        }
-        return Response(data=data)
+        categories = models.FoodCategory.objects.all()    
+        serializer = serializers.FoodListSerializer(categories, many=True)
+        return Response(serializer.data)
+ 
